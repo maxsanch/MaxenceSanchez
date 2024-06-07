8433a89c77e6c435ad0a7419de54a6071b2915d0
Maxence_Sanchez <maxence.sanchez-varas-leclercq@uha.fr>
2024-06-07 11:58:29 +0200

    time to lunch

--- script/projet.js
+++ script/projet.js
@@ -1,91 +1,76 @@
-let projets = ""
+let link = new URLSearchParams(location.search);
+let rat = link.get('selection');
 
-for (let test in SAE['Web']) {
-    projets += "<div class='projet2'><a href='projetsdétail.html?domaine=Web&projet="+test+"'><div class='titrehaut'><h1>"+SAE['Web'][test]['titre']+"</h1></div><div class='centreinfo'><div class='informations'>"+SAE['Web'][test]['description']+"</div></div></a><a href='projetsdétail.html?domaine=Web&projet="+test+"'><div class='imagesposition'><div class='centre'><div class='imagecentre'><img src='../img/imagesprojets/Web/"+test+" 1.png' alt='rer'></div><div class='abso1'><img src='../img/imagesprojets/Web/"+test+" 2.png' alt='rer'></div><div class='abso2'><img src='../img/imagesprojets/Web/"+test+" 3.png' alt='rer'></div></div></div></a></div>"
-}
-
-document.querySelector('.total').innerHTML = projets
-
-function web() {
+if (rat == "Web") {
     let projets = ""
 
     for (let test in SAE['Web']) {
-        projets += "<a href='projetsdétail.html?domaine=Web&projet="+test+"'><div class='projet2'><div class='titrehaut'><h1>"+SAE['Web'][test]['titre']+"</h1></div><div class='centreinfo'><div class='informations'>"+SAE['Web'][test]['description']+"</div></div><div class='imagesposition'><div class='centre'><div class='imagecentre'><img src='../img/imagesprojets/Web/"+test+" 1.png' alt='rer'></div><div class='abso1'><img src='../img/imagesprojets/Web/"+test+" 2.png' alt='rer'></div><div class='abso2'><img src='../img/imagesprojets/Web/"+test+" 3.png' alt='rer'></div></div></div></div></a>"
+        projets += "<a href='projetsdétail.html?domaine=Web&projet=" + test + "'><div class='projet2'><div class='titrehaut'><h1>" + SAE['Web'][test]['titre'] + "</h1></div><div class='centreinfo'><div class='informations'>" + SAE['Web'][test]['description'] + "</div></div><div class='imagesposition'><div class='centre'><div class='imagecentre'><img src='../img/imagesprojets/Web/" + test + " 1.png' alt='rer'></div><div class='abso1'><img src='../img/imagesprojets/Web/" + test + " 2.png' alt='rer'></div><div class='abso2'><img src='../img/imagesprojets/Web/" + test + " 3.png' alt='rer'></div></div></div></div></a>"
     }
 
     document.querySelector('.total').innerHTML = projets
+    document.querySelector('.choixproj:nth-child(1)').style.color = "#E52165"
 }
 
-
-designe = document.querySelector('.choixproj:nth-child(3)')
-designe.addEventListener('click', video)
-
-function video() {
+if (rat == "video") {
     let projets = ""
 
     for (let test in SAE['vidéo']) {
-        projets += "<a href='projetsdétail.html?domaine=vidéo&projet="+test+"'><div class='projet2'><div class='titrehaut'><h1>"+SAE['vidéo'][test]['titre']+"</h1></div><div class='centreinfo'><div class='informations'>"+SAE['vidéo'][test]['description']+"</div></div><div class='imagesposition'><div class='centre'><div class='imagecentre'><img src='../img/imagesprojets/vidéo/"+test+" 1.png' alt='rer'></div><div class='abso1'><img src='../img/imagesprojets/vidéo/"+test+" 3.png' alt='rer'></div><div class='abso2'><img src='../img/imagesprojets/vidéo/"+test+" 2.png' alt='rer'></div></div></div></div></a>"
+        projets += "<a href='projetsdétail.html?domaine=vidéo&projet=" + test + "'><div class='projet2'><div class='titrehaut'><h1>" + SAE['vidéo'][test]['titre'] + "</h1></div><div class='centreinfo'><div class='informations'>" + SAE['vidéo'][test]['description'] + "</div></div><div class='imagesposition'><div class='centre'><div class='imagecentre'><img src='../img/imagesprojets/vidéo/" + test + " 1.png' alt='rer'></div><div class='abso1'><img src='../img/imagesprojets/vidéo/" + test + " 3.png' alt='rer'></div><div class='abso2'><img src='../img/imagesprojets/vidéo/" + test + " 2.png' alt='rer'></div></div></div></div></a>"
     }
 
     document.querySelector('.total').innerHTML = projets
+    document.querySelector('.choixproj:nth-child(3)').style.color = "#E52165"
 }
 
-designe = document.querySelector('.choixproj:nth-child(1)')
-designe.addEventListener('click', web)
 
 
-
-designe = document.querySelector('.choixproj:nth-child(2)')
-designe.addEventListener('click', design)
-
-function design() {
+if (rat == "web design") {
     let projets = ""
 
     for (let test in SAE['web design']) {
-        projets += "<a href='projetsdétail.html?domaine=web design&projet="+test+"'><div class='projet2'><div class='titrehaut'><h1>"+SAE['web design'][test]['titre']+"</h1></div><div class='centreinfo'><div class='informations'>"+SAE['web design'][test]['description']+"</div></div><div class='imagesposition'><div class='centre'><div class='imagecentre'><img src='../img/imagesprojets/web design/"+test+" 1.png' alt='rer'></div><div class='abso1'><img src='../img/imagesprojets/web design/"+test+" 2.png' alt='rer'></div><div class='abso2'><img src='../img/imagesprojets/web design/"+test+" 3.png' alt='rer'></div></div></div></div></a>"
+        projets += "<a href='projetsdétail.html?domaine=web design&projet=" + test + "'><div class='projet2'><div class='titrehaut'><h1>" + SAE['web design'][test]['titre'] + "</h1></div><div class='centreinfo'><div class='informations'>" + SAE['web design'][test]['description'] + "</div></div><div class='imagesposition'><div class='centre'><div class='imagecentre'><img src='../img/imagesprojets/web design/" + test + " 1.png' alt='rer'></div><div class='abso1'><img src='../img/imagesprojets/web design/" + test + " 2.png' alt='rer'></div><div class='abso2'><img src='../img/imagesprojets/web design/" + test + " 3.png' alt='rer'></div></div></div></div></a>"
     }
 
     document.querySelector('.total').innerHTML = projets
+    document.querySelector('.choixproj:nth-child(2)').style.color = "#E52165"
+    
 }
 
-designe = document.querySelector('.choixproj:nth-child(4)')
-designe.addEventListener('click', graphisme)
-
-function graphisme() {
+if (rat == "graphisme") {
     let projets = ""
 
     for (let test in SAE['graphisme']) {
-        projets += "<a href='projetsdétail.html?domaine=graphisme&projet="+test+"'><div class='projet2'><div class='titrehaut'><h1>"+SAE['graphisme'][test]['titre']+"</h1></div><div class='centreinfo'><div class='informations'>"+SAE['graphisme'][test]['description']+"</div></div><div class='imagesposition'><div class='centre'><div class='imagecentre'><img src='../img/imagesprojets/graphisme/"+test+" 1.png' alt='rer'></div><div class='abso1'><img src='../img/imagesprojets/graphisme/"+test+" 3.png' alt='rer'></div><div class='abso2'><img src='../img/imagesprojets/graphisme/"+test+" 2.png' alt='rer'></div></div></div></div></a>"
+        projets += "<a href='projetsdétail.html?domaine=graphisme&projet=" + test + "'><div class='projet2'><div class='titrehaut'><h1>" + SAE['graphisme'][test]['titre'] + "</h1></div><div class='centreinfo'><div class='informations'>" + SAE['graphisme'][test]['description'] + "</div></div><div class='imagesposition'><div class='centre'><div class='imagecentre'><img src='../img/imagesprojets/graphisme/" + test + " 1.png' alt='rer'></div><div class='abso1'><img src='../img/imagesprojets/graphisme/" + test + " 3.png' alt='rer'></div><div class='abso2'><img src='../img/imagesprojets/graphisme/" + test + " 2.png' alt='rer'></div></div></div></div></a>"
     }
 
     document.querySelector('.total').innerHTML = projets
+    document.querySelector('.choixproj:nth-child(4)').style.color = "#E52165"
 }
 
 
-designe = document.querySelector('.choixproj:nth-child(5)')
-designe.addEventListener('click', divers)
-
-function divers() {
+if (rat == "divers") {
     let projets = ""
 
     for (let test in SAE['divers']) {
-        projets += "<a href='projetsdétail.html?domaine=divers&projet="+test+"'><div class='projet2'><div class='titrehaut'><h1>"+SAE['divers'][test]['titre']+"</h1></div><div class='centreinfo'><div class='informations'>"+SAE['divers'][test]['description']+"</div></div><div class='imagesposition'><div class='centre'><div class='imagecentre'><img src='../img/imagesprojets/divers/"+test+" 1.png' alt='rer'></div><div class='abso1'><img src='../img/imagesprojets/divers/"+test+" 2.png' alt='rer'></div><div class='abso2'><img src='../img/imagesprojets/divers/"+test+" 3.png' alt='rer'></div></div></div></div></a>"
+        projets += "<a href='projetsdétail.html?domaine=divers&projet=" + test + "'><div class='projet2'><div class='titrehaut'><h1>" + SAE['divers'][test]['titre'] + "</h1></div><div class='centreinfo'><div class='informations'>" + SAE['divers'][test]['description'] + "</div></div><div class='imagesposition'><div class='centre'><div class='imagecentre'><img src='../img/imagesprojets/divers/" + test + " 1.png' alt='rer'></div><div class='abso1'><img src='../img/imagesprojets/divers/" + test + " 2.png' alt='rer'></div><div class='abso2'><img src='../img/imagesprojets/divers/" + test + " 3.png' alt='rer'></div></div></div></div></a>"
     }
 
     document.querySelector('.total').innerHTML = projets
+    document.querySelector('.choixproj:nth-child(5)').style.color = "#E52165"
 }
 
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     var header = document.querySelector("header");
-  
-    window.addEventListener("scroll", function() {
-      var scrollPosition = window.scrollY;
-  
-      if (scrollPosition > 50) {
-        header.classList.add("scroll"); 
-      } else {
-        header.classList.remove("scroll");
-      }
+
+    window.addEventListener("scroll", function () {
+        var scrollPosition = window.scrollY;
+
+        if (scrollPosition > 50) {
+            header.classList.add("scroll");
+        } else {
+            header.classList.remove("scroll");
+        }
     });
 });
