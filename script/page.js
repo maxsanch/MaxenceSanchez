3a9bc476790e48fe8093312688f32e177e90ffef
maxsanch <maxence.sanchez-varas-leclercq@uha.fr>
2024-06-18 18:59:45 +0200

    patch de bugs

--- script/page.js
+++ script/page.js
@@ -51,12 +51,11 @@ var oui = document.querySelector(".menutel");
 var deroulant = document.querySelector(".deroutel")
 
 oui.addEventListener("click", function() {
+  header.classList.toggle("header2")
     if(deroulant.classList.contains('deroutelo')){
       deroulant.classList.remove("deroutelo");
-      document.querySelector("header").classList.remove("header2");
     }
     else{
       deroulant.classList.add("deroutelo");
-      document.querySelector("header").classList.add("header2");
     }
   });
