2ca07bff51972369b5736ccefa983542838bc12b
maxsanch <maxence.sanchez-varas-leclercq@uha.fr>
2024-06-19 12:51:13 +0200

    test

--- script/data.js
+++ script/data.js
@@ -2,30 +2,30 @@ const SAE = {
   "vidéo": {
     "projet 1": {
       "titre": "Vidéo analyse de films en anglais.",
-      "description": "<p>J'ai été amené à réaliser une vidéo en anglais dans le cadre d'un travail de fin de semestre, en l'occurrence ici pour mon premier semestre de cours en IUT dans ma formation BUT MMI.</p><p> Le but était de produire une vidéo pour des personnes cinéphiles analysant unt thème ou un auteur-compositeur au choix. </p><p>Pour notre part, nous avons choisi la dystopie dans les films futuriste car ces thèmes nous intéréssaient.</p><p> Moi et mon équipe, composée de 4 membres pour ce projet, avons finalement rendu une vidéo mettant en scène les différents univers analysés.</p>",
-      "étapes": "nous avons réspécté plusieures étapes afin d'assurer un rendu de qualitée, dans un premeier temps, nous avons du réalsier un découpage technique, par la suite, nosu avons du réaliser le tournage de la vidéo afin de, par la suite, monter cette dernière."
+      "description": "<p>J'ai été amené à réaliser une vidéo en anglais dans le cadre d'un travail de fin de semestre, en l'occurrence ici pour mon premier semestre de cours en IUT dans ma formation BUT MMI.</p><p> Le but était de produire une vidéo pour des personnes cinéphiles analysant un thème ou un auteur-compositeur au choix. </p><p>Pour notre part, nous avons choisi la dystopie dans les films futuristes car ces thèmes nous intéressaient.</p><p> Moi et mon équipe, composée de 4 membres pour ce projet, avons finalement rendu une vidéo mettant en scène les différents univers analysés.</p>",
+      "étapes": "<p>Nous avons réspecté plusieurs étapes afin d'assurer un rendu de qualité, dans un premier temps, nous avons dû réaliser un découpage technique, par la suite, nous avons dû réaliser le tournage de la vidéo afin de, par la suite, monter cette dernière.</p>"
     },
     "projet 2": {
       "titre": "Présentation du vélo générateur d'électricité",
-      "description": "En tant que second projet officiel en vidéo, j'ai dû réaliser une vidéo présentant le vélo générateur d'électricité réalisé par le lycée bugatti dans le but de le présenter par la suite, à Sélestat, nous avons alors chacun du, au sein de notre promotion réaliser cette vidéo afin que l'une d'entre elles soit projetée le jour de la conférence en parlant, à Sélestat. J'ai pour ma part réalisé une vidéo présentant chaque aspect du vélo sous forme de question réponse, une manière de traiter le sujet de manière ludique et accessible à tous.",
-      "étapes": "Comme beaucoup de projets ou de publicités vidéo, plusieurs étapes ont été nécessaires, dans un premier temps, réaliser un découpage technique en analysant les attendus du client, ici, le lycée Bugatti, puis, par la suite, rédiger notre texte explicatif dans un langage accessible à tous, vulgarisé. Enfin les rushs ont étés pris et partagés au sein de la promotion afin que, par la suite, chaque personne ai pu réaliser individuellement ou en binôme le montage de la vidéo."
+      "description": "<p>En tant que second projet officiel en vidéo, j'ai dû réaliser une vidéo présentant le vélo générateur d'électricité réalisé par le lycée Bugatti dans le but de le présenter par la suite, à Sélestat, nous avons alors chacun dû, au sein de notre promotion réaliser cette vidéo afin que l'une d'entre elles soit projetée le jour de la conférence en parlant, à Sélestat.</p><p> J'ai pour ma part réalisé une vidéo présentant chaque aspect du vélo sous forme de question réponse, une manière de traiter le sujet de manière ludique et accessible à tous.</p>",
+      "étapes": "<p>Comme beaucoup de projets ou de publicités vidéo, plusieurs étapes ont été nécessaires, dans un premier temps, réaliser un découpage technique en analysant les attendus du client, ici, le lycée Bugatti.</p><p> Par la suite, j'ai dû rédiger notre texte explicatif dans un langage accessible à tous, vulgarisé.</p><p> Enfin les rushs ont étés pris et partagés au sein de la promotion afin que, par la suite, chaque personne ai pu réaliser individuellement ou en binôme le montage de la vidéo.</p>"
     },
   },
 
   "web design": {
     "projet 1": {
       "titre": "Entrainement sur la page de Coca-Cola",
-      "description": "Pour ce projet j'ai réalisé une maquette figma de la page Coca-Cola tout en modifiant certains éléments afin de rendre la page un peu plus personnelle. Nous avons, tout au long du module avec notre enseignant, designer différents éléments qui ont dû être par la suite utilisé dans cette maquette réalisable sur figma ou Adobe XD au choix.",
-      "étapes": "Pour réaliser ce projet je suis passé par plusieurs étapes, dans un premier temps j'ai dû aller regarder la page du site Coca-Cola France afin de m'en inspirer et de la recréer, j'ai ensuite ajouté des éléments plus personnels tels que les carrousels et la manière de passer d'un élément à un autre, ou encore le haut de la page. j'ai dû par ailleur au préalable réaliser la bouteille sur illustrator afin de l'importer sur mon site."
+      "description": "<p>Pour ce projet j'ai réalisé une maquette Figma de la page Coca-Cola tout en modifiant certains éléments afin de rendre la page un peu plus personnelle.</p><p> Nous avons, tout au long du module avec notre enseignant, designer différents éléments qui ont dû être par la suite utilisé dans cette maquette réalisable sur Figma ou Adobe XD au choix.</p>",
+      "étapes": "<p>Pour réaliser ce projet je suis passé par plusieurs étapes, dans un premier temps j'ai dû aller regarder la page du site Coca-Cola France afin de m'en inspirer et de la recréer, j'ai ensuite ajouté des éléments plus personnels tels que les carrousels et la manière de passer d'un élément à un autre, ou encore le haut de la page.</p><p> j'ai dû par ailleur au préalable réaliser la bouteille sur illustrator afin de l'importer sur mon site.</p>"
     },
     "projet 2": {
       "titre": "Maquette pour le site la tanière des loups",
-      "description": "Ce projet partait d'une mise en scène en cours dans laquelle un client, ici, notre enseignant nous à donné pour consigne de réaliser la maquette de son site parmi 3 thèmes. Nous avons alors choisi celui nous inspirant le plus, la tanière des loups, en compagnie de mon binome nous avosn alors dû interoger le client afin d'analyser ses attendus pour, par la suite, construire la maquette attendue",
-      "étapes": "Afin de créer cette maquette nous avons du réaliser plusieurs étapes. Dans un premier temps nosyu avons du préparer des questions à poser au client, partant à l'origine de seulement un simple nom, nous avons alors ensuite éfféctué le rendez-vous avec le client afin de comprendre ses attentes, les analyser, et passer enfin à la réalisation de la maquette."
+      "description": "<p>Ce projet partait d'une mise en scène en cours dans laquelle un client, ici, notre enseignant nous à donné pour consigne de réaliser la maquette de son site parmi 3 thèmes.</p><p> Nous avons alors choisi celui nous inspirant le plus, la tanière des loups, en compagnie de mon binôme nous avons alors dû interroger le client afin d'analyser ses attendus pour, par la suite, construire la maquette attendue.</p>",
+      "étapes": "<p>Afin de créer cette maquette nous avons dû réaliser plusieurs étapes.</p><p> Dans un premier temps nous avons dû préparer des questions à poser au client, partant à l'origine de seulement un simple nom, nous avons alors ensuite éffectué le rendez-vous avec le client afin de comprendre ses attentes, les analyser, et passer enfin à la réalisation de la maquette.</p>"
     },
     "projet 3": {
       "titre": "Passage du mode sombre au claire",
-      "description": "Nous avons dû réaliser sur figma une maquette d'un site trouvé sur dribble afin de, par la suite, le passer en mode claire.",
+      "description": "Nous avons dû réaliser sur Figma une maquette d'un site trouvé sur dribble afin de, par la suite, le passer en mode claire.",
       "étapes": "Dans un premier temps j'ai alors eu besoin de recréer le site original sur Figma, par la suite, j'ai passé les différents objets en clair en me basant sur des bonnes pratiques."
     },
   },
@@ -33,47 +33,47 @@ const SAE = {
   "Web": {
     "projet 1": {
       "titre": "Mon premier site portfolio",
-      "description": "Pour ce projet j'ai réalisé mon premier site portfolio, ce dernier a été réalisé dans le cadre d'un projet scolaire en fin de semestre à gros coefficient, il s'agissait donc d'expliquer chaque compétences développées en détails. ces compétences étaient regroupées sous forme d'ACs ou apprentissages critiques, les renseignements de ce portfolio n'étant pas accordés à ma vision d'un site vitrine, j'ai préféré recréer un site web portfolio regroupant de nouveaux travaux et les mettant en scène. Voici <a href='#'><u>le lien</u> </a> de ce dernier",
-      "étapes": "Dans un premier temps j'ai dû, pour ce projet, rédiger les textes que je comptais incruster dans ce portfolio, les justifications étant bien plus importantes, par la suite, j'ai réalisé la maquette de ce dernier dans le but d'être plus efficace lors du développement de mon site. Enfin après la mise en place de ma banque de données en Javascript, j'ai pu passer au développement de ce dernier."
+      "description": "<p>Pour ce projet j'ai réalisé mon premier site portfolio, ce dernier a été réalisé dans le cadre d'un projet scolaire en fin de semestre à gros coefficient, il s'agissait donc d'expliquer chaque compétences développées en détails.</p><p> Ces compétences étaient regroupées sous forme d'ACs ou apprentissages critiques, les renseignements de ce portfolio n'étant pas accordés à ma vision d'un site vitrine, j'ai préféré recréer un site web portfolio regroupant de nouveaux travaux et les mettant en scène.</p><p> Voici <a href='https://maxsanch.github.io/portfolio'><u>le lien</u> </a> de ce dernier<p>",
+      "étapes": "<p>Dans un premier temps j'ai dû, pour ce projet, rédiger les textes que je comptais incruster dans ce portfolio, les justifications étant bien plus importantes, ensuite, j'ai réalisé la maquette de ce dernier dans le but d'être plus efficace lors du développement de mon site.</p><p> Enfin après la mise en place de ma banque de données en Javascript, j'ai pu passer au développement de ce dernier.</p>"
     },
     "projet 2": {
       "titre": "Site WordPress pour un festival de musique",
-      "description": "Dans le cadre de la communication 360 d'un festival de musique fictif, nous avons été amené à produire un site web présentant ce dernier, il s'agissait pour notre part, à moi et mon groupe de 4 personnes, de créer un festival de rock et de métal sur le thème d'Halloween. Nous avons alors dû utiliser des couleurs majoritairement sombres dans le thème d'Halloween correspondant à notre charte graphique.",
-      "étapes": "Après avoir trouvé le thème du festival, sa plateforme de marque et sa manière d'être, j'ai pu réaliser une maquette figma afin de, par la suite, développer mon site sur Wordpress."
+      "description": "<p>Dans le cadre de la communication 360 d'un festival de musique fictif, nous avons été amené à produire un site web présentant ce dernier, il s'agissait pour notre part, à moi et mon groupe de 4 personnes, de créer un festival de rock et de métal sur le thème d'Halloween.</p><p> Nous avons alors dû utiliser des couleurs majoritairement sombres dans le thème d'Halloween correspondant à notre charte graphique.</p>",
+      "étapes": "<p>Après avoir trouvé le thème du festival, sa plateforme de marque et sa manière d'être, j'ai pu réaliser une maquette Figma afin de, par la suite, développer mon site sur WordPress.</p>"
     },
     "projet 3": {
       "titre": "Site god of war",
-      "description": "Nous avons dû réaliser au sein de mon second semestre de BUT MMI, un site dynamique sur le thème de notre choix, c'est alors que j'ai choisi le thème du jeu god of war que j'apprécie beaucoup, nosu avons alors utilisé des langages tels que PHP ou SQL ainsi qu'une base de données dans le but de rendre notre site dynamique, via, par exemple, un système de commentaires.",
-      "étapes": "Après avoir trouvé le thème du festival, sa plateforme de marque et sa manière d'être, j'ai pu réaliser une maquette Figma afin de, par la suite, développer mon site sur WordPress."
+      "description": "<p>Nous avons dû réaliser au sein de mon second semestre de BUT MMI, un site dynamique sur le thème de notre choix, c'est alors que j'ai choisi le thème du jeu god of war que j'apprécie beaucoup, nous avons alors utilisé des langages tels que PHP ou SQL ainsi qu'une base de données dans le but de rendre notre site dynamique, via, par exemple, un système de commentaires.</p>",
+      "étapes": "<p>Après avoir trouvé le thème du festival, sa plateforme de marque et sa manière d'être, j'ai pu réaliser une maquette Figma afin de, par la suite, développer mon site sur WordPress.</p>"
     },
   },
   "graphisme": {
     "projet 1": {
       "titre": "Production de motifs en tous genres",
-      "description": "Lors d'un nouveau projet de fin de semestre nous avons eu à \"sauver le MISE\", le musée d'impression sur étoffes <br> nous avons alors dû choisir un thème, pour ma part, les insectes volants, et créer par la suite des motifs afin de remplacer ceux ayant été volé au MISE",
-      "étapes": "Dans un premier temps j'ai du produire des planches d'inspiration afin de trouver une bonne idée, par la suite, nous avons réalisé les motifs sur illustrator afin de, enfin, les mettre en situation par le billet de différents mockups."
+      "description": "<p>Lors d'un nouveau projet de fin de semestre nous avons eu à \"sauver le MISE\", le musée d'impression sur étoffes <br> nous avons alors dû choisir un thème, pour ma part, les insectes volants, et créer par la suite des motifs afin de remplacer ceux ayant été volé au MISE. </p>",
+      "étapes": "Dans un premier temps j'ai dû produire des planches d'inspiration afin de trouver une bonne idée, par la suite, nous avons réalisé les motifs sur illustrator afin de, enfin, les mettre en situation par le billet de différents mockups."
     },
     "projet 2": {
       "titre": "réalisation d'un logo pour une agence de communication",
-      "description": "Dans le cadre du projet de communication 360 d'un festival de musique, j'ai été ammené à réaliser un logo sur illustrator pour l'agence de communication s'occupant de la campagne de communication de ce festival. Ce logo est celui d'une agence de communication dans l'événementiel, j'ai également dû trouver un nom et créer cette agence. Le nom de lumicom et l'ampoule liée à la lumière sont venue de ma représentation de l'idée (utilisée principalement dans les cartoons).",
-      "étapes": "Après la réalisation des planches d'inspiration, j'ai réalisé le logo sur illsutrator, j'ai ensuite du justifier dans le document chacun de mes choix afin d'être pret pour le rendu."
+      "description": "<p>Dans le cadre du projet de communication 360 d'un festival de musique, j'ai été ammené à réaliser un logo sur illustrator pour l'agence de communication s'occupant de la campagne de communication de ce festival.</p><p> Ce logo est celui d'une agence de communication dans l'événementiel, j'ai également dû trouver un nom et créer cette agence. Le nom de lumicom et l'ampoule liée à la lumière sont des idées venues de ma représentation de l'idée (utilisée principalement dans les cartoons).</p>",
+      "étapes": "<p>Après la réalisation des planches d'inspiration, j'ai réalisé le logo sur illsutrator, j'ai ensuite du justifier dans le document chacun de mes choix afin d'être prêt pour le rendu.</p>"
     },
     "projet 3": {
       "titre": "réalisation d'un logo pour une boisson énergisante",
-      "description": "Dans le cadre du projet de communication 360 d'un festival de musique, j'ai été ammené à réaliser un logo sur illustrator pour l'agence de communication s'occupant de la campagne de communication de ce festival. Ce logo est celui d'une agence de communication dans l'événementiel, j'ai également dû trouver un nom et créer cette agence. Le nom de lumicom et l'ampoule liée à la lumière est venue de ma représentatiojn de l'idée (utilisée principalement dans les cartoons)",
-      "étapes": "Après la réalisation des planches d'inpiration, j'ai réalisé le logo sur illsutrator, j'ai ensuite du justifier dans le document chacun de mes choix afin d'être pret pour le rendu."
+      "description": "<p>Dans le cadre du projet de communication sur une boisson énergétique, j'ai été ammené à réaliser le logo pour cette dernière, j'ai alors choisi de créer un logo représentant un phoenix, une mise à jour de l'ancien logo proposé.</p><p> Le phoenix qui renaît de ses cendres afin de créer Fire Up, les couleurs montrent le côté écoresponsable par le vert mais aussi l'énergie par le orange.</p>",
+      "étapes": "<p>J'ai d'abord proposé des esquisses au crayon afin de visualiser le logo final, par la suite je suis allé sur Illustrator afin de réaliser le logo final de la boisson.</p>"
     },
   },
   "divers": {
     "projet 1": {
       "titre": "création d'une fake news dans le cadre d'un projet.",
-      "description": "Le but de cet exercice était de créer une fake news pour des enfants agés entre 8 et 13 ans, le but était que cette dernière soit crédible pour que, la page suivante, une leçon de fact cheking soit donnée.",
-      "étapes": "Après avoir trouvé l'idée principale, adaptée pour le public visé, j'ai pu commencer à rédiger la fake news, par la suite, mon binôme avec lequel j'étais pour cette fake news, nommée Marie Raffath à réaliser la mise en page de cette dernière."
+      "description": "<p>Le but de cet exercice était de créer une fake news pour des enfants agés entre 8 et 13 ans, le but était que cette dernière soit crédible pour que, la page suivante, une leçon de fact cheking soit donnée.</p>",
+      "étapes": "<p>Après avoir trouvé l'idée principale, adaptée pour le public visé, j'ai pu commencer à rédiger la fake news, par la suite, mon binôme avec lequel j'étais pour cette fake news, nommée Marie Raffath à réaliser la mise en page de cette dernière.</p>"
     },
     "projet 2": {
       "titre": "analyse d'un site web sous différents angles.",
-      "description": "Nous avons dû analyser un site web sur plusieurs aspects, technique, ergonomique, design, sémiologique, contextuel.",
-      "étapes": "Les analyses citées précédemments ont été faites une par une, nous avons pu également faire une analyse SEO de ce site durant le processus.",
+      "description": "<p>Nous avons dû analyser un site web sur plusieurs aspects, technique, ergonomique, design, sémiologique, contextuel.</p>",
+      "étapes": "<p>Les analyses citées précédemments ont été faites une par une, nous avons pu également faire une analyse SEO de ce site durant le processus.</p>",
     },
   },
 }
